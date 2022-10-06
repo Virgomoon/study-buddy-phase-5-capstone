2d768f62a02a54d2d28cdd2deae86ec464bd4892
Joseph Guillory <jagmanx99@yahoo.com>
2022-10-05 18:38:15 -0700

    User notes controller works

--- app/controllers/usernotes_controller.rb
+++ app/controllers/usernotes_controller.rb
@@ -1,7 +1,8 @@
 class UsernotesController < ApplicationController
 
     def show
-        buddy = @current_user.users.find(params[:id])
-        render json: buddy.notes
+        buddy = @current_user.buddies.find(params[:id])
+        # see_buddy = buddy_notes buddy
+        render json: buddy.buddy.notes.to_json(except: [:users])
     end
 end
